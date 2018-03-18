4710419957ea56116bb8827af89f7fd50aa6c7d0
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2018-03-18 12:17:49 +0200

    Update single_14.cpp

--- lab 1/single_14.cpp
+++ lab 1/single_14.cpp
@@ -11,27 +11,27 @@ struct node {
 };
 
 int main() {
-    // EX 14 FALSE right A \ B and implement with looped list
-		cout <<endl << "EX -- 14"<<endl;
-		list team1, team2, new_team1, new_team2; // creating lists
-		int size = 10; // size of the lists
-		for(int i = 1; i <= size; i++) { // initializing the lists with some values
-			team1.createnode(i);
-			team2.createnode(i);
-		}
-		ex14_ = team1.copy();
+   	 // EX
+	cout <<endl << "EX -- 14"<<endl;
+	list team1, team2, new_team1, new_team2; // creating lists
+	int size = 10; // size of the lists
+	for(int i = 1; i <= size; i++) { // initializing the lists with some values
+		team1.createnode(i);
+		team2.createnode(i);
+	}
+	ex14_ = team1.copy();
   
-		int n = 4;// assign n
-		int m = 5;// assign m
-		new_team1 = lot(team1,n);
-    new_team2 = lot(team2,m);
+	int n = 4;// assign n
+	int m = 5;// assign m
+	new_team1 = lot(team1,n);
+    	new_team2 = lot(team2,m);
     
-    // results
-    cout << "Team 1:" << endl;
-		new_team1.display();
-		cout << endl <<"Team 2:" << endl;
-		new_team2.display();
-		return 0;
+    	// results
+   	cout << "Team 1:" << endl;
+	new_team1.display();
+	cout << endl <<"Team 2:" << endl;
+	new_team2.display();
+	return 0;
 }
 
 list lot(list x,int n) {
