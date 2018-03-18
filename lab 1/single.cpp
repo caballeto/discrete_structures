276789b93ed3dc27070479fb5c1ad4e5cbd3cc1e
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2018-03-18 10:40:48 +0200

    Update single.cpp

--- lab 1/single.cpp
+++ lab 1/single.cpp
@@ -1,5 +1,6 @@
 #include "single.hpp"
 
+// get the number of elements in list
 int list::length() {
 	node * current = head;
 
@@ -20,7 +21,7 @@ void list::to_loop() {
 	}
 	temp->next = join;
 }
-
+// EX 12 erase list
 void list::erase() {
 	node *current = head;
 	node *next;
@@ -32,6 +33,7 @@ void list::erase() {
 	head = NULL;
 }
 
+// EX 11 sort in decreasing order
 void list::sort_decreasing() {
 	node * current = head;
 		int temp_data;
@@ -75,7 +77,8 @@ void list::delete_every_k(int k) {
 	}
 }
 
-void list::delete_every_third() {
+// EX 10 
+void list::delete_every_third() { // deleting every third element
 	if(head == NULL)
 		cout << "list is empty" << endl;
 	node *current = head;
@@ -93,6 +96,7 @@ void list::delete_every_third() {
 	}
 }
 
+// EX 9 delete every second element in list
 void list::delete_every_second() {
 	if(head == NULL)
 		cout << "list is empty" << endl;
@@ -111,6 +115,7 @@ void list::delete_every_second() {
 	}
 }
 
+// EX 8 sort elements of ist in increasing order
 void list::sort_increasing() {
 	node * current = head;
 	int temp_data;
@@ -136,6 +141,7 @@ void list::sort_increasing() {
 	}
 }
 
+// makes an intersection of two given lists 
 list list::intersection(node *current1, node *current2) {
 	list intersected;
 	node *temp = new node;
@@ -153,13 +159,17 @@ list list::intersection(node *current1, node *current2) {
 	return intersected;
 }
 
+// return the head node of the list
 node* list::get_head() {
 	return head;
 }
 
+// return the tail node of the list
 node* list::get_tail() {
 	return tail;
 }
+
+// EX 4 concat two lists
 void list::concat_list(node *current, node *b) {
 	if(current != NULL && b != NULL) {
 		if(current->next == NULL)
@@ -168,6 +178,8 @@ void list::concat_list(node *current, node *b) {
 			concat_list(current->next,b);
 	}
 }
+
+// EX 2 returns a copy of the lists
 list list::copy() {
 	list copied;
 	node *temp=new node;
@@ -179,6 +191,7 @@ list list::copy() {
 	return copied;
 }
 
+// move element from pos to pos + n
 void list::move_element(int pos, int n) {
 	node *prev = new node;
 	node *current = new node;
@@ -196,6 +209,7 @@ void list::move_element(int pos, int n) {
 	}
 }
 
+// add element to the end of the list
 void list::createnode(int value) {
   node *temp=new node;
   temp->data=value;
@@ -211,6 +225,7 @@ void list::createnode(int value) {
   }
 }
 
+// display the elements of the list
 void list::display() {
   node *temp=new node;
   temp=head;
@@ -219,6 +234,8 @@ void list::display() {
     temp=temp->next;
   }
 }
+
+// insert an element at the start of the list
 void list::insert_start(int value) {
   node *temp=new node;
   temp->data=value;
@@ -226,6 +243,7 @@ void list::insert_start(int value) {
   head=temp;
 }
 
+// insert element before position pos
 void list::insert_position_before(int pos, int value) {
   node *pre=new node;
   node *cur=new node;
@@ -240,6 +258,7 @@ void list::insert_position_before(int pos, int value) {
   temp->next=cur;
 }
 
+// EX 6 insert element after position pos
 void list::insert_position_after(int pos, int value) {
   node *pre=new node;
   node *cur=new node;
@@ -273,9 +292,10 @@ void list::delete_last() {
   }
   tail=previous;
   previous->next=NULL;
-  delete current;
+  delete current;	
 }
 
+// EX 5 delete element at position pos
 void list::delete_position(int pos) {
   node *current=new node;
   node *previous=new node;
