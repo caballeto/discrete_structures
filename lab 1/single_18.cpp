ceb7d8a3a0c96bd4e6b28e4b0a9a40622e58f149
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2018-03-18 12:57:53 +0200

    Update single_18.cpp

--- lab 1/single_18.cpp
+++ lab 1/single_18.cpp
@@ -13,37 +13,37 @@ struct node {
 
 int main() {
   	//EX 18
-		list all; // list of all products
+	list all; // list of all products
 	
-		//list determination
-		all.createnode(5);
-		all.createnode(5);
-		all.createnode(9);
-		all.createnode(4);
-		all.createnode(3);
-		all.createnode(7);
-		node *current = new node;
-		current = all.get_head();
-		current->made_by = "SONY";
-		current->next->made_by = "SONY";
-		current->next->next->made_by = "MICROSOFT";
-		current->next->next->next->made_by = "APPLE";
-		current->next->next->next->next->made_by = "MICROSOFT";
-		current->next->next->next->next->next->made_by = "SONY";
+	//list determination
+	all.createnode(5);
+	all.createnode(5);
+	all.createnode(9);
+	all.createnode(4);
+	all.createnode(3);
+	all.createnode(7);
+	node *current = new node;
+	current = all.get_head();
+	current->made_by = "SONY";
+	current->next->made_by = "SONY";
+	current->next->next->made_by = "MICROSOFT";
+	current->next->next->next->made_by = "APPLE";
+	current->next->next->next->next->made_by = "MICROSOFT";
+	current->next->next->next->next->next->made_by = "SONY";
 	
-		//main
-		list only_sony;
-		for(int i = 0; i < all.length(); i++) {
-			if(current->made_by == "SONY") {
-				only_sony.createnode(current->data);
-			}
-			current = current->next;
+	//main
+	list only_sony;
+	for(int i = 0; i < all.length(); i++) {
+		if(current->made_by == "SONY") {
+			only_sony.createnode(current->data);
 		}
+		current = current->next;
+	}
   
-		//results
-		cout << endl <<"EX 18 result: " << endl;
-		all.display();
-		cout << endl<< "ONLY SONY" << endl;
-		only_sony.display();
-    return 0;
+	//results
+	cout << endl <<"EX 18 result: " << endl;
+	all.display();
+	cout << endl<< "ONLY SONY" << endl;
+	only_sony.display();
+	return 0;
 }
