02657f1816dc553aa0c87540391d78ed24cabd73
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2018-03-18 13:13:13 +0200

    Update single_20.cpp

--- lab 1/single_20.cpp
+++ lab 1/single_20.cpp
@@ -10,23 +10,23 @@ struct node {
 
 
 int main() {
-    // EX 20
-		cout <<endl << "EX -- 20"<<endl;
-		list list1, list2;
-		int total = 20;//  total number of products
-		for(int i = 1; i <= total; i++) { // initializing two lists with some values
-			list1.createnode(i+2);
-			list2.createnode(i);
-		}
-		list made_both; // result list
-		made_both = list1.intersection(list1.get_head(), list2.get_head()); // finding the intersection of 2 lists
+    	// EX 20
+	cout <<endl << "EX -- 20"<<endl;
+	list list1, list2;
+	int total = 20;//  total number of products
+	for(int i = 1; i <= total; i++) { // initializing two lists with some values
+		list1.createnode(i+2);
+		list2.createnode(i);
+	}
+	list made_both; // result list
+	made_both = list1.intersection(list1.get_head(), list2.get_head()); // finding the intersection of 2 lists
   
-    // results
-		cout << endl <<"EX 20 result: " << endl;
-		list1.display();
-		cout << endl;
-		list2.display();
-		cout << endl;
-		made_both.display(); // made by both
-    return 0;
+    	// results
+	cout << endl <<"EX 20 result: " << endl;
+	list1.display();
+	cout << endl;
+	list2.display();
+	cout << endl;
+	made_both.display(); // made by both
+    	return 0;
 }
