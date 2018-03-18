f7a8bf0c763f2abc91b8685e1a67335d1610865a
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2018-03-18 12:53:00 +0200

    Update single_17.cpp

--- lab 1/single_17.cpp
+++ lab 1/single_17.cpp
@@ -11,26 +11,26 @@ struct node {
 
 int main() {
   	//EX 17
-		list list1, list2, result;
-		int k = 14; // number of students
-		int e = 4; // number of tickets for exam
-		for(int i = 1; i <= k; i++) {
-			list1.createnode(i);
-		}
-		for(int i = 1; i <= e; i++) {
-			list2.createnode(i);
-		}
-		list2.to_loop(); // making a loop in list2
-    
-    int count = 0;
-    node *temp = list2.get_head();
-    while(count != k) {ticket
-      result.createnode(temp->data); // Result is a list: student on position i got ticket number i->data
-      temp = temp->next;
-      count++;
-    }
+	list list1, list2, result;
+	int k = 14; // number of students
+	int e = 4; // number of tickets for exam
+	for(int i = 1; i <= k; i++) {// initializing list with some values
+		list1.createnode(i);
+	}
+	for(int i = 1; i <= e; i++) {
+		list2.createnode(i);
+	}
+	list2.to_loop(); // making a loop in list2
   
-	// results :
+	int count = 0;
+	node *temp = list2.get_head();
+	while(count != k) {
+	  result.createnode(temp->data); // Result is a list: student on position i got ticket number i->data
+	  temp = temp->next;
+	  count++;
+	}
+  
+  	// results :
 	cout <<endl << "EX -- 17 results:"<<endl;
 	result.display();
   return 0;
