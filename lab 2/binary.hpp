8d2ea37bc138aa6945a00cce5ee2c6f21f4ed46a
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2018-03-28 17:55:58 +0300

    Create binary.hpp

---
+++ lab 2/binary.hpp
@@ -0,0 +1,15 @@
+struct node {
+	int value;
+	char b;
+	node *left;
+	node *right;
+};
+
+class btree {
+	public:
+	    node *root;
+		btree();
+		node* insert(int, node*);
+		void inorder(node*);
+		void make_attributes(node*, int);
+};
